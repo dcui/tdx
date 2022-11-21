2f4b4fedfcdec77f647c191b39b8ab202fa8c603
Dexuan Cui <decui@microsoft.com>
2022-11-21 08:35:39 +0000

    Drivers: hv: vmbus: Support TDX guests
    Intel folks added the generic code to support a TDX guest in April, 2022.
    This commit and some earlier commits from me add the Hyper-V specific
    code so that a TDX guest can run on Hyper-V.
    
    Signed-off-by: Dexuan Cui <decui@microsoft.com>

--- arch/x86/mm/pat/set_memory.c
+++ arch/x86/mm/pat/set_memory.c
@@ -2120,7 +2120,7 @@ static int __set_memory_enc_pgtable(unsigned long addr, int numpages, bool enc)
 
 static int __set_memory_enc_dec(unsigned long addr, int numpages, bool enc)
 {
-	if (hv_is_isolation_supported())
+	if (hv_is_isolation_supported() && !hv_isolation_type_tdx())
 		return hv_set_mem_host_visibility(addr, numpages, !enc);
 
 	if (cc_platform_has(CC_ATTR_MEM_ENCRYPT))
