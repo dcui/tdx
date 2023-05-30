dcui/tdx

commit 5ee2e838169009bd50b2322379a707c8e62ab637

x86/hyperv: Fix hyperv_pcpu_input_arg handling when CPUs go online/offline
These commits

a494aef23dfc ("PCI: hv: Replace retarget_msi_interrupt_params with hyperv_pcpu_input_arg")
2c6ba4216844 ("PCI: hv: Enable PCI pass-thru devices in Confidential VMs")

update the Hyper-V virtual PCI driver to use the hyperv_pcpu_input_arg
because that memory will be correctly marked as decrypted or encrypted
for all VM types (CoCo or normal). But problems ensue when CPUs in the
VM go online or offline after virtual PCI devices have been configured.

When a CPU is brought online, the hyperv_pcpu_input_arg for that CPU is
initialized by hv_cpu_init() running under state CPUHP_AP_ONLINE_DYN.
But this state occurs after state CPUHP_AP_IRQ_AFFINITY_ONLINE, which
may call the virtual PCI driver and fault trying to use the as yet
uninitialized hyperv_pcpu_input_arg. A similar problem occurs in a CoCo
VM if the MMIO read and write hypercalls are used from state
CPUHP_AP_IRQ_AFFINITY_ONLINE.

When a CPU is taken offline, IRQs may be reassigned in state
CPUHP_TEARDOWN_CPU. Again, the virtual PCI driver may fault trying to
use the hyperv_pcpu_input_arg that has already been freed by a
higher state.

Fix the onlining problem by adding state CPUHP_AP_HYPERV_ONLINE
immediately after CPUHP_AP_ONLINE_IDLE (similar to CPUHP_AP_KVM_ONLINE)
and before CPUHP_AP_IRQ_AFFINITY_ONLINE. Use this new state for
Hyper-V initialization so that hyperv_pcpu_input_arg is allocated
early enough.

Fix the offlining problem by not freeing hyperv_pcpu_input_arg when
a CPU goes offline. Retain the allocated memory, and reuse it if
the CPU comes back online later.

Signed-off-by: Michael Kelley <mikelley@microsoft.com>
(backported from commit d2d07a52edd573b7a884d30f90f347a10d83fba0 https://github.com/kelleymh/linux.git)
[rtg - conflicts with existing TDX patches]
Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

drivers/hv/hv_common.c
@@ -135,9 +135,6 @@ int hv_common_cpu_init(unsigned int cpu)
 	flags = irqs_disabled() ? GFP_ATOMIC : GFP_KERNEL;
 
 	inputarg = (void **)this_cpu_ptr(hyperv_pcpu_input_arg);
-	*inputarg = kmalloc(pgcount * HV_HYP_PAGE_SIZE, flags);
-	if (!(*inputarg))
-		return -ENOMEM;
 
 	if (hv_isolation_type_tdx()) {
 		ret = set_memory_decrypted((unsigned long)*inputarg, pgcount);
@@ -150,9 +147,19 @@ int hv_common_cpu_init(unsigned int cpu)
 		memset(*inputarg, 0x00, pgcount * HV_HYP_PAGE_SIZE);
 	}
 
-	if (hv_root_partition) {
-		outputarg = (void **)this_cpu_ptr(hyperv_pcpu_output_arg);
-		*outputarg = (char *)(*inputarg) + HV_HYP_PAGE_SIZE;
+	/*
+	 * hyperv_pcpu_input_arg and hyperv_pcpu_output_arg memory is already
+	 * allocated if this CPU was previously online and then taken offline
+	 */
+	if (!*inputarg) {
+		*inputarg = kmalloc(pgcount * HV_HYP_PAGE_SIZE, flags);
+		if (!(*inputarg))
+			return -ENOMEM;
+
+		if (hv_root_partition) {
+			outputarg = (void **)this_cpu_ptr(hyperv_pcpu_output_arg);
+			*outputarg = (char *)(*inputarg) + HV_HYP_PAGE_SIZE;
+		}
 	}
 
 	msr_vp_index = hv_get_register(HV_REGISTER_VP_INDEX);
@@ -167,27 +174,20 @@ int hv_common_cpu_init(unsigned int cpu)
 
 int hv_common_cpu_die(unsigned int cpu)
 {
-	unsigned long flags;
-	void **inputarg, **outputarg;
 	void *mem;
 	int pgcount = hv_root_partition ? 2 : 1;
-	int ret;
+	void **inputarg;
+	unsigned long flags;
 
 	local_irq_save(flags);
 
 	inputarg = (void **)this_cpu_ptr(hyperv_pcpu_input_arg);
 	mem = *inputarg;
-	*inputarg = NULL;
-
-	if (hv_root_partition) {
-		outputarg = (void **)this_cpu_ptr(hyperv_pcpu_output_arg);
-		*outputarg = NULL;
-	}
 
 	local_irq_restore(flags);
 
 	if (hv_isolation_type_tdx()) {
-		ret = set_memory_encrypted((unsigned long)mem, pgcount);
+		int ret = set_memory_encrypted((unsigned long)mem, pgcount);
 		if (ret)
 			pr_warn("Hyper-V: Failed to encrypt input arg on cpu%d: %d\n",
 				cpu, ret);
@@ -195,7 +195,17 @@ int hv_common_cpu_die(unsigned int cpu)
 		return 0;
 	}
 
-	kfree(mem);
+	/*
+	 * The hyperv_pcpu_input_arg and hyperv_pcpu_output_arg memory
+	 * is not freed when the CPU goes offline as the hyperv_pcpu_input_arg
+	 * may be used by the Hyper-V vPCI driver in reassigning interrupts
+	 * as part of the offlining process.  The interrupt reassignment
+	 * happens *after* the CPUHP_AP_HYPERV_ONLINE state has run and
+	 * called this function.
+	 *
+	 * If a previously offlined CPU is brought back online again, the
+	 * originally allocated memory is reused in hv_common_cpu_init().
+	 */
 
 	return 0;
 }

arch/x86/hyperv/hv_init.c
@@ -429,7 +429,7 @@ void __init hyperv_init(void)
 			goto free_vp_assist_page;
 	}
 
-	cpuhp = cpuhp_setup_state(CPUHP_AP_ONLINE_DYN, "x86/hyperv_init:online",
+	cpuhp = cpuhp_setup_state(CPUHP_AP_HYPERV_ONLINE, "x86/hyperv_init:online",
 				  hv_cpu_init, hv_cpu_die);
 	if (cpuhp < 0)
 		goto free_ghcb_page;

include/linux/cpuhotplug.h
@@ -205,6 +205,7 @@ enum cpuhp_state {
 
 	/* Online section invoked on the hotplugged CPU from the hotplug thread */
 	CPUHP_AP_ONLINE_IDLE,
+	CPUHP_AP_HYPERV_ONLINE,
 	CPUHP_AP_SCHED_WAIT_EMPTY,
 	CPUHP_AP_SMPBOOT_THREADS,
 	CPUHP_AP_X86_VDSO_VMA_ONLINE,

commit e9b9172554f02188f0a94a40da2df02a7567fc8d

Fix the backported commit 5ee2e8381690
The Ubuntu kernel here doesn't have CPUHP_AP_KVM_ONLINE (which is introduced
by [1], which can't be cherry-picked cleanly here). IMO as long as
CPUHP_AP_HYPERV_ONLINE comes before CPUHP_AP_IRQ_AFFINITY_ONLINE
CPUHP_AP_IRQ_AFFINITY_ONLINE, we should be good, i.e. Tim's backport for
CPUHP_AP_HYPERV_ONLINE should be correct.

The backported code in hv_common_cpu_init() and hv_common_cpu_die() is
not quite right. Fix it.

[1] The mainline aaf12a7b4323 ("KVM: Rename and move CPUHP_AP_KVM_STARTING to ONLINE section")

Fixes: 5ee2e8381690 ("x86/hyperv: Fix hyperv_pcpu_input_arg handling when CPUs go online/offline")
Signed-off-by: Dexuan Cui <decui@microsoft.com>

drivers/hv/hv_common.c
@@ -136,17 +136,6 @@ int hv_common_cpu_init(unsigned int cpu)
 
 	inputarg = (void **)this_cpu_ptr(hyperv_pcpu_input_arg);
 
-	if (hv_isolation_type_tdx()) {
-		ret = set_memory_decrypted((unsigned long)*inputarg, pgcount);
-		if (ret) {
-			/* It may be unsafe to free *inputarg */
-			*inputarg = NULL;
-			return ret;
-		}
-
-		memset(*inputarg, 0x00, pgcount * HV_HYP_PAGE_SIZE);
-	}
-
 	/*
 	 * hyperv_pcpu_input_arg and hyperv_pcpu_output_arg memory is already
 	 * allocated if this CPU was previously online and then taken offline
@@ -155,6 +144,17 @@ int hv_common_cpu_init(unsigned int cpu)
 		*inputarg = kmalloc(pgcount * HV_HYP_PAGE_SIZE, flags);
 		if (!(*inputarg))
 			return -ENOMEM;
+
+		if (hv_isolation_type_tdx()) {
+			ret = set_memory_decrypted((unsigned long)*inputarg, pgcount);
+			if (ret) {
+				/* It may be unsafe to free *inputarg */
+				*inputarg = NULL;
+				return ret;
+			}
+
+			memset(*inputarg, 0x00, pgcount * HV_HYP_PAGE_SIZE);
+		}
 
 		if (hv_root_partition) {
 			outputarg = (void **)this_cpu_ptr(hyperv_pcpu_output_arg);
@@ -174,27 +174,6 @@ int hv_common_cpu_init(unsigned int cpu)
 
 int hv_common_cpu_die(unsigned int cpu)
 {
-	void *mem;
-	int pgcount = hv_root_partition ? 2 : 1;
-	void **inputarg;
-	unsigned long flags;
-
-	local_irq_save(flags);
-
-	inputarg = (void **)this_cpu_ptr(hyperv_pcpu_input_arg);
-	mem = *inputarg;
-
-	local_irq_restore(flags);
-
-	if (hv_isolation_type_tdx()) {
-		int ret = set_memory_encrypted((unsigned long)mem, pgcount);
-		if (ret)
-			pr_warn("Hyper-V: Failed to encrypt input arg on cpu%d: %d\n",
-				cpu, ret);
-		/* It's unsafe to free 'mem'. */
-		return 0;
-	}
-
 	/*
 	 * The hyperv_pcpu_input_arg and hyperv_pcpu_output_arg memory
 	 * is not freed when the CPU goes offline as the hyperv_pcpu_input_arg